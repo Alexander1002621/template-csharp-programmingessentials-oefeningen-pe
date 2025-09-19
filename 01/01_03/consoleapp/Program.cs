c6c1949b22d475d656909193fbb7e04a23fb3aa4
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 14:03:05 +0200

    3

--- 01/01_03/consoleapp/Program.cs
+++ 01/01_03/consoleapp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int aantalVolwassenen, aantalKinderen;
+double eindBedrag;
+
+Console.WriteLine("voer het aantal volwassene in:");
+aantalVolwassenen = int.Parse(Console.ReadLine());
+Console.WriteLine("voer het aantal kinderen onder 12 jaar in:");
+aantalKinderen = int.Parse(Console.ReadLine());
+
+eindBedrag = (aantalVolwassenen * 10) + (aantalKinderen * 7.5);
+
+eindBedrag = Math.Round(eindBedrag, 0);
+
+Console.WriteLine($"Totaal te betalen:{eindBedrag} euro");
+Console.ReadLine();
