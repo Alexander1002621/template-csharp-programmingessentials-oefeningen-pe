9675177d08006d9f97fe6c4eed5d5a506e49de50
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 17:00:29 +0200

    4

--- 01/01_04/consoleapp/Program.cs
+++ 01/01_04/consoleapp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double TotaalteBetalen, totaalBedrag, prijs, btwBedrag;
+int nationaleOp, internationaleOp, alleOproepen;
+
+internationaleOp = int.Parse(Console.ReadLine());
+nationaleOp = int.Parse(Console.ReadLine());
+
+
+alleOproepen = nationaleOp + internationaleOp;
+prijs = alleOproepen * 0.12;
+totaalBedrag = prijs + 23;
+btwBedrag = totaalBedrag * 21 / 100;
+TotaalteBetalen = totaalBedrag + btwBedrag;
+
+Console.WriteLine($" totaal te betalen: {TotaalteBetalen:N2}  euro");
