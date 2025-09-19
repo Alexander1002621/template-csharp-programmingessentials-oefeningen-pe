a0e5bfa089c5612f266acbd0e3ce2335c71914b1
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 17:07:34 +0200

    1

--- 02/02_01/consoleapp/Program.cs
+++ 02/02_01/consoleapp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal1, getal2, totaal;
+
+Console.Write("Geef 1 getal in : ");
+getal1 = int.Parse(Console.ReadLine());
+Console.Write("Geef 1 getal in : ");
+getal2 = int.Parse(Console.ReadLine());
+
+if (getal1 >= getal2)
+{
+    totaal = getal1 - getal2;
+
+    Console.WriteLine($"{getal1} - {getal2} = {totaal}. ");
+}
+else
+{
+    totaal = getal2 - getal1;
+
+    Console.WriteLine($"{getal2} - {getal1} = {totaal}. ");
+}
+
+Console.WriteLine("Druk op Enter om af the sluiten");
+Console.ReadLine();
