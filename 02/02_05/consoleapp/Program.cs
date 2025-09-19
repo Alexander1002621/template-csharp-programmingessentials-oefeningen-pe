19c6a66a4e5f8e1b6791e49bd1c4acbcd7ea926f
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 17:21:20 +0200

    2.5

--- 02/02_05/consoleapp/Program.cs
+++ 02/02_05/consoleapp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int landingsplaats, punten;
+
+
+landingsplaats = int.Parse(Console.ReadLine());
+
+if (landingsplaats == 1)
+{
+    punten = 0;
+}
+else if (landingsplaats == 2)
+{
+    punten = 20;
+}
+else if (landingsplaats == 3)
+{
+    punten = 50;
+}
+else
+{
+    punten = 100;
+}
+Console.WriteLine($"{punten} punten.");
