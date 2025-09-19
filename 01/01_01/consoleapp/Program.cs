e4bf4aad35f3bf86a64edce66fea15886eaf4984
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 13:25:14 +0200

    1.1 gemaakt

--- 01/01_01/consoleapp/Program.cs
+++ 01/01_01/consoleapp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal1, getal2, getal3, getal4, som, product;
+
+getal1 = int.Parse(Console.ReadLine());
+getal2 = int.Parse(Console.ReadLine());
+getal3 = int.Parse(Console.ReadLine());
+getal4 = int.Parse(Console.ReadLine());
+
+som = getal1 + getal3;
+product = getal2 * getal4;
+
+Console.WriteLine($"optelling: {som} ");
+Console.WriteLine($"vermenigvuldiging: {product}");
+
+Console.WriteLine("enter om af te sluiten");
+Console.ReadLine();
