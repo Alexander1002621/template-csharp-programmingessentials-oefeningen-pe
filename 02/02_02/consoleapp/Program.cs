e57a1280920890116854ca75637ce2e1a78b3cdf
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 17:09:55 +0200

    2.2

--- 02/02_02/consoleapp/Program.cs
+++ 02/02_02/consoleapp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal1, getal2, uitkomst;
+string resultaat = "";
+
+getal1 = int.Parse(Console.ReadLine());
+getal2 = int.Parse(Console.ReadLine());
+
+if (getal2 == 0)
+{
+    resultaat += $"{getal1} is niet deelbaar door 0";
+}
+
+else
+{
+    uitkomst = getal1 / getal2;
+    resultaat += $"{getal1} / {getal2} = {uitkomst}";
+}
+
+Console.WriteLine(resultaat);
+Console.ReadLine();
