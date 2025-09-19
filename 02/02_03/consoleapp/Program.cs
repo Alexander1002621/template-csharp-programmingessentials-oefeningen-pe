301ed4953afba48f469f927d156254bfde990db4
Alexander1002621 <r1002621@student.thomasmore.be>
2025-09-19 17:13:35 +0200

    2.3

--- 02/02_03/consoleapp/Program.cs
+++ 02/02_03/consoleapp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string voornaam, familienaam;
+int leeftijd;
+
+voornaam = Console.ReadLine();
+familienaam = Console.ReadLine();
+leeftijd = int.Parse(Console.ReadLine());
+
+if (leeftijd >= 18)
+{
+    Console.WriteLine($"{voornaam} {familienaam}: Volwassenen");
+}
+
+if (leeftijd < 18)
+{
+    Console.WriteLine($"{voornaam} {familienaam}: Jeugd");
+
+}
+
+
+Console.WriteLine("druk op enter om af te sluiten");
+Console.WriteLine();
